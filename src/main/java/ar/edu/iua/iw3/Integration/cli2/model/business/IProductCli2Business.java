b529a87e37ca141d5d39498db3b1c1649fae80c8
Agustin Rodeyro <89660035+agusclap@users.noreply.github.com>
2025-10-01 00:26:44 -0300

    Add CLI2 B2B product creation endpoint

--- src/main/java/ar/edu/iua/iw3/Integration/cli2/model/business/IProductCli2Business.java
+++ src/main/java/ar/edu/iua/iw3/Integration/cli2/model/business/IProductCli2Business.java
@@ -6,9 +6,11 @@ import java.util.List;
 import ar.edu.iua.iw3.integration.cli2.model.ProductCli2;
 import ar.edu.iua.iw3.integration.cli2.model.ProductCli2SlimView;
 import ar.edu.iua.iw3.model.business.BusinessException;
+import ar.edu.iua.iw3.model.business.FoundException;
 
 public interface IProductCli2Business {
     public List<ProductCli2> listExpired(Date since) throws BusinessException;
     public List<ProductCli2SlimView> listSlim() throws BusinessException;
     public List<ProductCli2> listByPrice(Double startPrice, Double endPrice) throws BusinessException;
+    public ProductCli2 add(ProductCli2 product) throws FoundException, BusinessException;
 }
